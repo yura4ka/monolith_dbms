52272ab7d9de6cee20364dab325566933b16545e
yura4ka <yura4ka05@gmail.com>
2024-10-15 15:08:24 +0300

    feat: add create database

--- monolith_dbms/Controllers/HomeController.cs
+++ monolith_dbms/Controllers/HomeController.cs
@@ -6,47 +6,77 @@ using System.Diagnostics;
 
 namespace monolith_dbms.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly IConnectionManager _connectionManager;
-		private readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        private readonly IConnectionManager _connectionManager;
+        private readonly ILogger<HomeController> _logger;
 
-		public HomeController(IConnectionManager connectionManager, ILogger<HomeController> logger)
-		{
-			_connectionManager = connectionManager;
-			_logger = logger;
-		}
+        public HomeController(IConnectionManager connectionManager, ILogger<HomeController> logger)
+        {
+            _connectionManager = connectionManager;
+            _logger = logger;
+        }
 
-		public IActionResult Index()
-		{
-			var files = Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\dbs"), "*.db")
-								   .Select(f => new DbFileModel { Name = Path.GetFileName(f), FullPath = f })
-								   .ToList();
-			return View(files);
-		}
+        public IActionResult Index()
+        {
+            var files = GetDatabases();
+            return View(files);
+        }
 
-		[HttpPost]
-		public IActionResult Connect(string fullPath)
-		{
-			string id = _connectionManager.CreateConnection(fullPath);
-			return RedirectToAction("Index", "Database", new { id });
-		}
+        [HttpPost]
+        public IActionResult Connect(string fullPath)
+        {
+            string id = _connectionManager.CreateConnection(fullPath);
+            return RedirectToAction("Index", "Database", new { id });
+        }
 
-		[HttpPost]
-		public IActionResult Delete(string fullPath)
-		{
-			if (System.IO.File.Exists(fullPath))
-			{
-				System.IO.File.Delete(fullPath);
-			}
+        [HttpPost]
+        public IActionResult Delete(string fullPath)
+        {
+            if (System.IO.File.Exists(fullPath))
+            {
+                System.IO.File.Delete(fullPath);
+            }
 
-			return RedirectToAction("Index");
-		}
+            return RedirectToAction("Index");
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(NewDbModel newDb)
+        {
+            string fullPath = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\dbs", newDb.Name ?? "");
+            if (!fullPath.EndsWith(".db")) fullPath += ".db";
+            if (Path.Exists(fullPath))
+            {
+                ModelState.AddModelError("Name", "Already exists");
+            }
+
+            if (ModelState.IsValid)
+            {
+                string id = _connectionManager.CreateConnection(fullPath);
+                return RedirectToAction("Index", "Database", new { id });
+            }
+
+            return View(newDb);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private IEnumerable<DbFileModel> GetDatabases()
+        {
+            return Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\dbs"), "*.db")
+                .Select(f => new DbFileModel { Name = Path.GetFileName(f), FullPath = f })
+                .ToList();
+        }
+    }
 }
---
+++ monolith_dbms/Models/ViewModels/NewDbModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace monolith_dbms.Models.ViewModels
+{
+    public class NewDbModel
+    {
+        [Required]
+        public string? Name { get; set; }
+    }
+
+}
